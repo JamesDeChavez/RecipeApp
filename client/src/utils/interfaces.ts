a4c45c4e178b54e7b91a9f5879fed07b6ef6be9f
JamesDeChavez <jamesdechavez23@gmail.com>
2022-07-28 00:51:51 -0700

    added interfaces for registration and login

--- client/src/utils/interfaces.ts
+++ client/src/utils/interfaces.ts
@@ -10,9 +10,43 @@ export interface Post {
     authorId: string
 }
 
+export interface RegisterInputs{
+    username: string,
+    email: string,
+    password: string,
+    confirmPW: string
+}
+
+export interface RegisterValidChecks{
+    username: boolean,
+    email: boolean,
+    password: boolean,
+    confirmPW: boolean
+}
+
+export interface RegisterFocus{
+    username: boolean,
+    email: boolean,
+    password: boolean,
+    confirmPW: boolean
+}
+
+export interface LoginInputs{
+    username: string,
+    password: string
+}
+
 export interface Ingredient {
     _id?: string,
     name: string,
     brand: string,
     include?: boolean
+}
+
+export interface Video {
+    _id?: string,
+    title: string,
+    thumbnail: string,
+    channel: string,
+    videoId: string
 }
